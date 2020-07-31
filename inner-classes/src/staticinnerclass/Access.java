6ba29b5da11ef9364c85cf247d84702d84080e38
Ajay-The-Programmer <ajaydevkar612@gmail.com>
2020-07-31 17:29:52 +0530

    Static Inner Class and Static member

---
+++ inner-classes/src/staticinnerclass/Access.java
@@ -0,0 +1,39 @@
+package staticinnerclass;
+
+
+
+class Outer{
+	
+	static void m1()
+	{
+		System.out.println("Outer-m1");
+	}
+
+	static class Inner{
+		
+		static void m2() {
+			
+			System.out.println("Inner-m2");
+			
+		}
+		
+	}
+	
+}
+
+public class Access {
+
+	public static void main(String[] args) {
+		
+		Outer.m1();
+		Outer.Inner.m2();
+
+	
+	
+
+	
+	}
+
+}
+
+
