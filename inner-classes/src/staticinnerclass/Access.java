ebad582def302fc46c7e70d8dbfc7a39f1953f67
Ajay-The-Programmer <ajaydevkar612@gmail.com>
2020-07-31 17:46:27 +0530

    Static Inner Class and non static method

--- inner-classes/src/staticinnerclass/Access.java
+++ inner-classes/src/staticinnerclass/Access.java
@@ -11,7 +11,7 @@ class Outer{
 
 	static class Inner{
 		
-		static void m2() {
+		void m2() {
 			
 			System.out.println("Inner-m2");
 			
@@ -26,8 +26,9 @@ public class Access {
 	public static void main(String[] args) {
 		
 		Outer.m1();
-		Outer.Inner.m2();
-
+		Outer.Inner inner=new Outer.Inner();
+		
+		inner.m2();
 	
 	
 
