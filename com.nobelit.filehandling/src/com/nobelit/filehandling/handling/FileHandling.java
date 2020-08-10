bfa59956ea7da2dec456e71f0b58108ff310f835
Ajay-The-Programmer <ajaydevkar612@gmail.com>
2020-08-10 11:15:25 +0530

    FileInputStream read succesfully

--- com.nobelit.filehandling/src/com/nobelit/filehandling/handling/FileHandling.java
+++ com.nobelit.filehandling/src/com/nobelit/filehandling/handling/FileHandling.java
@@ -1,15 +1,32 @@
 package com.nobelit.filehandling.handling;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FileHandling {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		FileInputStream file = null;
-		file = new FileInputStream("/home/ajay/myjavaprograms/com.nobelit.filehandling/src/com/nobelit/filehandling/handling/FileHandling.java");
-		if (file != null) {
-			System.out.println("FileFound");
+	public static void main(String[] args) throws IOException {
+		FileInputStream obj = null;
+
+		try {
+			obj = new FileInputStream(
+					"/home/ajay/myjavaprograms/com.nobelit.filehandling/src/com/nobelit/filehandling/handling/FileHandling.java");
+			System.out.println("FIle Opened");
+			
+			int x;
+			while ((x=obj.read())!=-1) {
+
+				System.out.print((char)x);
+
+			}
+
+		} finally {
+
+			if (obj != null) {
+				obj.close();
+				System.out.println("FileClose");
+			}
+
 		}
 
 	}
